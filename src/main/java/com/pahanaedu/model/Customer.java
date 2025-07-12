22f59c87dd99867f1e010e1efa6afd1f78bfab5a
thevidumithmal <thevidumithmal@gmail.com>
2025-07-12 18:54:15 +0530

    Second Commit

---
+++ src/main/java/com/pahanaedu/model/Customer.java
@@ -0,0 +1,49 @@
+package com.pahanaedu.model;
+
+public class Customer {
+    private int accountNumber;
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private int unitsConsumed;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getUnitsConsumed() {
+        return unitsConsumed;
+    }
+
+    public void setUnitsConsumed(int unitsConsumed) {
+        this.unitsConsumed = unitsConsumed;
+    }
+}
