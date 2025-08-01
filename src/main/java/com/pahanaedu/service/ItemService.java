04d7674f787f285c315209a409196a308c780587
thevidumithmal <thevidumithmal@gmail.com>
2025-08-01 20:02:12 +0530

    16 commit

--- src/main/java/com/pahanaedu/service/ItemService.java
+++ src/main/java/com/pahanaedu/service/ItemService.java
@@ -1,11 +1,14 @@
 package com.pahanaedu.service;
 
 import com.pahanaedu.dao.ItemDao;
+import com.pahanaedu.dto.ItemDTO;
+import com.pahanaedu.mapper.ItemMapper;
 import com.pahanaedu.model.Item;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ItemService {
     private final ItemDao itemDao;
@@ -14,19 +17,25 @@ public class ItemService {
         this.itemDao = new ItemDao(connection);
     }
 
-    public boolean addItem(Item item) throws SQLException {
+    public boolean addItem(ItemDTO itemDTO) throws SQLException {
+        Item item = ItemMapper.toModel(itemDTO);
         return itemDao.addItem(item);
     }
 
-    public List<Item> getAllItems() throws SQLException {
-        return itemDao.getAllItems();
+    public List<ItemDTO> getAllItems() throws SQLException {
+        List<Item> items = itemDao.getAllItems();
+        return items.stream()
+                .map(ItemMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
-    public Item getItemById(int id) throws SQLException {
-        return itemDao.getItemById(id);
+    public ItemDTO getItemById(int id) throws SQLException {
+        Item item = itemDao.getItemById(id);
+        return ItemMapper.toDTO(item);
     }
 
-    public boolean updateItem(Item item) throws SQLException {
+    public boolean updateItem(ItemDTO itemDTO) throws SQLException {
+        Item item = ItemMapper.toModel(itemDTO);
         return itemDao.updateItem(item);
     }
 
